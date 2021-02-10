6b4e561f32816b54ec5e39064e89ab280ea3bf23
Roberto Godinez Robles <muere28@hotmail.com>
2021-02-10 04:07:14 -0600

    Vista de Restaurate

---
+++ Prueba/Client/Repositorios/IRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prueba.Client.Repositorios
+{
+    interface IRepositorio
+    {
+        Task<HttpResponseWrapper<object>> Post<T>(string url, T enviar);
+        Task<HttpResponseWrapper<T>> Get<T>(string url);
+    }
+}
---
+++ Prueba/Client/Repositorios/Repositorio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Prueba.Client.Repositorios
+{
+    public class Repositorio:IRepositorio
+    {
+        private readonly HttpClient httpClient;
+
+        public Repositorio(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        private JsonSerializerOptions OpcionesPorDefectoJSON =>
+            new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+        private async Task<T> DeserializarRespuesta<T>(HttpResponseMessage httpResponse, JsonSerializerOptions jsonSerializerOptions)
+        {
+            var responseString = await httpResponse.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(responseString, jsonSerializerOptions);
+        }
+
+        async Task<HttpResponseWrapper<object>> IRepositorio.Post<T>(string url, T enviar)
+        {
+            var enviarJSON = JsonSerializer.Serialize(enviar);
+            var enviarContent = new StringContent(enviarJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await httpClient.PostAsync(url, enviarContent);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+        async Task<HttpResponseWrapper<T>> IRepositorio.Get<T>(string url)
+        {
+            var responseHTTP = await httpClient.GetAsync(url);
+
+            if (responseHTTP.IsSuccessStatusCode)
+            {
+                var response = await DeserializarRespuesta<T>(responseHTTP, OpcionesPorDefectoJSON);
+                return new HttpResponseWrapper<T>(response, false, responseHTTP);
+            }
+            else
+            {
+                return new HttpResponseWrapper<T>(default, true, responseHTTP);
+            }
+        }
+    }
+}
